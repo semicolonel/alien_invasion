b93b9a3dfa9a8be1b3864e0dc4cdad1f157b1901
semicolonel <hiren.nandigama@gmail.com>
2020-09-23 23:22:46 +0530

    Added right & left movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,7 +6,7 @@ from  settings import Settings
 
 from ship import Ship
 
-class AlineInvasion:
+class AlienInvasion:
     """Overall class to manage assets and game behaviour"""
 
     def __init__(self):
@@ -20,25 +20,39 @@ class AlineInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
-        
+  
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            #Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
-            self.ship.blitme()
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-            #Make the most recently drawn screen visible
-            pygame.display.flip()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #Make a game instance and run the game
-    ai = AlineInvasion()
+    ai = AlienInvasion()
     ai.run_game()
